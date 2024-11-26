8f3ee69432c7338957aaa2152ae08e2cabe1b1fb
Lord Stoker <tpc00@iesemilidarder.com>
2024-11-26 21:29:15 +0100

    static components and their CSS

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+        <img src="images/chef-claude-icon.png" alt="Chef" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
