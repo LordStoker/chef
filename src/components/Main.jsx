3d00aa99e1269656671160850f57b054010ac19a
Lord Stoker <tpc00@iesemilidarder.com>
2024-12-11 12:49:53 +0100

    setState ingredients FA dependencies

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,36 @@
 import "./Main.css";
-
+import { useState } from "react";
 export default function Main() {
+    const [ingredients, setIngredients] = useState(["Chicken", "Salt", "Pepper", "Olive Oil"]);
+    
+    const ingredientsListItems = ingredients.map((ingredient, index) => (
+        <li key={index}>{ingredient}</li>
+    ))
+    
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log("Form Submitted")
+        const formData = new FormData(e.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        console.log(newIngredient);
+        setIngredients([...ingredients, newIngredient]);
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit ={handleSubmit}className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name = "ingredient"
                 />
                 <button>Add ingredient</button>
-            </form>
+                </form>
+                <ul>
+                    {ingredientsListItems}
+                </ul>
+            
         </main>
     );
 }
