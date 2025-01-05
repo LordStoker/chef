db1b7f033adfaf46f17af595d010a22b9b4d4858
Lord Stoker <tpc00@iesemilidarder.com>
2025-01-05 11:33:23 +0100

    smooth scroll to view

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,19 +3,30 @@ import "./Main.css";
 import { useState } from "react";
 import ClaudeRecipe from "./ClaudeRecipe";
 import {getRecipeFromMistral} from "../ai.js";
+import {useRef} from "react";
+import { useEffect } from "react";
 
 
 export default function Main() {
     const [ingredients, setIngredients] = useState([]);
     const [recipe, setRecipe] = useState("");
+    const recipeSection = useRef(null);
     const ingredientsListItems = ingredients.map((ingredient, index) => (
         <li key={index}>{ingredient}</li>
     ))
+    // Scroll to the recipe section when the recipe is ready
+    useEffect(() => {
+        if (recipeSection.current && recipe) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"});
+        }
+    }, [recipe]);
 
+    // Get the recipe from Mistral when the user is ready
     async function getRecipe() {
         const recipeMarkdown = await getRecipeFromMistral(ingredients);
         setRecipe(recipeMarkdown);        
     }
+    // Add an ingredient to the list
     function addIngredient(formData){
         const newIngredient = formData.get("ingredient");
         setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
@@ -35,6 +46,7 @@ export default function Main() {
                 </form>
                 {ingredientsListItems.length > 0 &&
                     <IngredientsList 
+                    ref={recipeSection}
                     ingredients={ingredientsListItems}
                     recipeReady={getRecipe}
                 />
